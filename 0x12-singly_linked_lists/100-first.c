fce83b7e0eb3868bf5b74fb0b116d6e48043ad43
UmaruJay <umarjalloh13@gmail.com>
2023-11-17 12:14:15 -0800

    0x12. C - Singly linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+void first(void) _attribute_ ((constructor));
+
+/**
+ * frist - prints a sentence before the main
+ * function is executed
+ */
+void first(void)
+{
+printf("you're beat! and yet. you must allow.\n")
+printf("I bore my house upon my back!\n");
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+0x12-singly_linked_lists 0.Print list 1. List length
